acd14420a93b82ab7cc3d60a2910655e2c827946
Vojtěch Porteš <vojtech.portes@arbes.com>
2019-08-08 15:32:21 +0200

    Build

--- webpack.prod.js
+++ webpack.prod.js
@@ -6,7 +6,7 @@ module.exports = {
   mode: "production",
   target: "node",
   output: {
-    path: path.resolve(__dirname, "build"),
+    path: path.resolve(__dirname, "dist"),
     filename: "index.js"
   },
   resolve: {
