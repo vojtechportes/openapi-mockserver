4df225e9953752994c378a2e0a00d1fcd109eac0
Vojtěch Porteš <vojtech.portes@arbes.com>
2019-08-08 16:40:38 +0200

    updates

--- bin/openapi-mockserver.js
+++ bin/openapi-mockserver.js
@@ -5,6 +5,7 @@ const cli = require('cli')
 const process = require('process')
 
 console.log('CWD: ', process.cwd())
+console.log(__dirname, __filename)
 
 cli.exec('node node_modules/openapi-mockserver/dist/index.js', (lines) => {
   console.log(lines)
